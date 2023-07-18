0bde3604b18116add348d9b50ac094d1b2558f54
Jesús Daniel Medina Cruz <jesus@beek.io>
2023-07-17 18:30:28 -0700

    Downgrade kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.application") version "8.0.2" apply false
     id("com.android.library") version "8.0.2" apply false
-    kotlin("android") version "1.8.21" apply false
-    kotlin("multiplatform") version "1.8.21" apply false
-    id("co.touchlab.faktory.kmmbridge") version "0.3.7" apply false
+    kotlin("android") version "1.7.22" apply false
+    kotlin("multiplatform") version "1.7.22" apply false
+    id("co.touchlab.faktory.kmmbridge") version "0.3.4" apply false
 }
 
 subprojects {
